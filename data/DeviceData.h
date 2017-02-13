6b0265d5f3fe21df5f6043b7b854a95957339eda
Daniel <danimanzaneque@gmail.com>
2017-02-13 14:02:07 +0100

    Added options to enable device type

--- data/DeviceData.h
+++ data/DeviceData.h
@@ -1,4 +1,8 @@
-#define ENABLE_SONOFF_TOUCH_ESP01
+// ENABLE YOUR DEVICE TYPE
+// #define ENABLE_SONOFF_TOUCH_ESP01 for "sonoff-touch-esp01"
+// #define ENABLE_SONOFF_TOUCH for "sonoff-touch"
+// #define ENABLE_SONOFF_S20 for "sonoff-s20"
+// #define ENABLE_SONOFF for "sonoff"
 
 #define DEVICE_NAME ""
 #define IP_NUMBER 000
