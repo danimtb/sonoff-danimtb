0e3b257d1ed267f7dc534e1c435fbbf770a2a4ca
danimtb <danimanzaneque@gmail.com>
2017-03-02 14:24:56 +0100

    Added read and write eeprom functions

--- lib/DataManager/DataManager.cpp
+++ lib/DataManager/DataManager.cpp
@@ -2,14 +2,52 @@
 
 DataManager::DataManager()
 {
-
+    EEPROM.begin(DM_TOTAL_MEM);
 }
+
+
+template <class T>
+bool DataManager::EEPROMwriteAnything(uint16_t startAddress, uint16_t maxMemory, const T& value)
+{
+    if (sizeof(value) > maxMemory)
+    {
+        return false;
+    }
+
+    const byte* p = (const byte*)(const void*)&value;
+
+    for (uint16_t i = 0; i < sizeof(value); i++)
+    {
+          EEPROM.write(startAddress++, *p++);
+    }
+
+    return true;
+}
+
+template <class T>
+void DataManager::EEPROMreadAnything(uint16_t startAddress, uint16_t maxMemory, T& value)
+{
+    byte* p = (byte*)(void*)&value;
+
+    for (uint16_t i = 0; i < maxMemory; i++)
+    {
+          *p++ = EEPROM.read(startAddress++);
+    }
+
+    return true;
+}
+
+void DataManager::setWifiSSID(std::string wifissid)
+{
+    this->EEPROMwriteAnything(DM_NETWORKDATA_ADDR_START, DM_WIFISSID_MEM, wifissid);
+}
+
 
 void DataManager::setup()
 {
 }
 
 
-void WifiManager::loop()
+void DataManager::loop()
 {
 }
--- lib/DataManager/DataManager.h
+++ lib/DataManager/DataManager.h
@@ -1,9 +1,10 @@
-#ifndef WIFIMANAGER_H
-#define WIFIMANAGER_H
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
 
 #include <Arduino.h>
 #include <EEPROM.h>
 
+#include <cstdint>
 #include <string>
 
 #define DM_BEGIN_ADDR 0
@@ -28,22 +29,53 @@
 
 #define DM_DEVICEDATA_ADDR_START 206
 #define DM_DEVICENAME_MEM 32
-#define DM_MQTTSTATUS_MEM 40
-#define DM_MQTTCOMMAND_MEM 40
-#define DM_MQTTSECOND_MEM 40
-#define DM_DEVICEDATA_ADDR_END 369
+#define DM_MQTTTOPIC1_MEM 40
+#define DM_MQTTTOPIC2_MEM 40
+#define DM_MQTTTOPIC3_MEM 40
+#define DM_MQTTTOPIC4_MEM 40
+#define DM_MQTTTOPIC5_MEM 40
+#define DM_MQTTTOPIC6_MEM 40
+#define DM_DEVICEDATA_ADDR_END 489
 
 class DataManager
 {
+private:
+    template <class T> bool EEPROMwriteAnything(uint16_t startAddress, uint16_t maxMemory, const T& value);
+    template <class T> void EEPROMreadAnything(uint16_t startAddress, uint16_t maxMemory, T& value);
 
 public:
     DataManager();
 
     void setWifiSSID(std::string wifissid);
     void setWifiPass(std::string wifipass);
-    void setIP(byte ip1, byte ip2, byte ip3, byte ip4);
-    void setMask(byte mask1, byte mask2, byte mask3, byte mask4);
-    void setGateway(byte gateway1, byte gateway2, byte gateway3, byte gateway4);
+    void setIP(uint8_t ip1, uint8_t ip2, uint8_t ip3, uint8_t ip4);
+    void setMask(uint8_t mask1, uint8_t mask2, uint8_t mask3, uint8_t mask4);
+    void setGateway(uint8_t gateway1, uint8_t gateway2, uint8_t gateway3, uint8_t gateway4);
+    void setOta(std::string ota);
+
+    void setMqttServer(std::string mqttServer);
+    void setMqttPort(uint16_t mqttPort);
+    void setMqttUser(std::string mqttUser);
+    void setMqttPass(std::string mqttPass);
+
+    void setDeviceName(std::string deviceName);
+    void setMqttTopic(uint8_t index, std::string mqttTopic);
+
+
+    std::string getWifiSSID();
+    std::string getWifiPass();
+    std::string getIP();
+    std::string getMask();
+    std::string getGateway();
+    std::string getOta();
+
+    std::string getMqttServer();
+    uint16_t getMqttPort();
+    std::string getMqttUser();
+    std::string getMqttPass();
+
+    std::string getDeviceName();
+    std::string getMqttTopic(uint8_t index);
 
     void setup();
 
