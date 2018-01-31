2969540f12270efa3f1b96f7c048514084cd236e
danimtb <danimanzaneque@gmail.com>
2018-01-31 20:28:00 +0100

    Raise if build not done

--- deploy.py
+++ deploy.py
@@ -2,12 +2,20 @@ import os
 import shutil
 from glob import glob
 
+# Set platfomio envirnoments to build
 envs = ["sonoff", "sonoff-s20", "sonoff-touch", "sonoff-touch-esp01", "sonoff-pow", "sonoff-switch",
         "sonoff-button"]
 
+# Get version of firmware from travis tag, otherwise set test-build
+version = os.getenv("TRAVIS_TAG", "test-build")
+
+# Iterate to generate firmware files and remove not needed ones
 for env in envs:
+    # Iterate to generate firmware files
     os.system("platformio run -e %s" % env)
     os.system("platformio run -e %s -t buildfs" % env)
+
+    # Remove not needed files
     shutil.rmtree(os.path.abspath(os.path.join(".pioenvs", env, "FrameworkArduino")))
     shutil.rmtree(os.path.abspath(os.path.join(".pioenvs", env, "src")))
     os.remove(os.path.abspath(os.path.join(".pioenvs", env, "libFrameworkArduino.a")))
@@ -16,10 +24,20 @@ for env in envs:
     for lib in glob(os.path.abspath(os.path.join(".pioenvs", env, "lib*"))):
          shutil.rmtree(os.path.abspath(os.path.join(".pioenvs", env, lib)))
 
+    # Check built files exist, otherwise raise
+    if (os.path.exists(os.path.abspath(os.path.join(".pioenvs", env, "firmware.bin"))) and
+        os.path.exists(os.path.abspath(os.path.join(".pioenvs", env, "firmware.elf"))) and
+        os.path.exists(os.path.abspath(os.path.join(".pioenvs", env, "spiffs.bin")))):
+        print ("OK [%s]: All files created" % env)
+    else:
+        raise Exception("ERROR [%s]: Missing files" % env)
+
+# Remove not needed external files
 for element in glob(os.path.abspath(os.path.join(".pioenvs", "*"))):
     if os.path.isfile(element):
         os.remove(element)
 
-shutil.make_archive(os.path.abspath("sonoff-danimtb_%s" % os.environ['TRAVIS_TAG']),
+# Create zip with firmware files for each platformio environment
+shutil.make_archive(os.path.abspath("sonoff-danimtb_%s" % version),
                     'zip',
                     os.path.abspath(os.path.join(".", ".pioenvs")))
